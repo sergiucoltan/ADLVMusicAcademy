a795d2e084dd698fc28d7f58de5662db3fd900f2
Sergiu Coltan <madsergoo@yahoo.com>
2021-04-06 12:19:41 +0300

    Add project files.

---
+++ ADLVMusicAcademy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADLVMusicAcademy.MvcApplication" Language="C#" %>
---
+++ ADLVMusicAcademy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ADLVMusicAcademy.Startup))]
+namespace ADLVMusicAcademy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
